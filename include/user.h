76314f5950fa9c8cc27bf0749d4e41e992d329df
Bardia-Sahami <bardia_sahami@yahoo.com>
2022-12-05 16:29:25 +0330

    Created User

--- include/user.h
+++ include/user.h
@@ -1,4 +1,25 @@
 #ifndef USER_H
 #define USER_H
 
+#include <iostream>
+#include "message.h"
+#include "crypto.h"
+#include "server.h"
+
+class Server;
+
+class User
+{
+public:
+    User(const std::string& _username, const std::string& _private_key, Server* _server);
+    const std::string& get_username() const;
+    bool send_text_message(const std::string& _text, const std::string& _receiver);
+    bool send_voice_message(const std::string& _receiver);
+private:
+    std::string username;     // username of the user
+    std::string private_key;  // private key of the user
+    Server* const server;     // a pointer to the server for communications
+};
+
+
 #endif //USER_H
--- src/user.cpp
+++ src/user.cpp
@@ -1,1 +1,42 @@
 #include "user.h"
+
+User::User(const std::string& _username, const std::string& _private_key, Server* _server):
+    username {_username},
+    private_key {_private_key},
+    server {_server}
+{
+    // std::cout << "User constructor\n";
+}
+
+const std::string& User::get_username() const
+{
+    return username;
+}
+
+bool User::send_text_message(const std::string& _text, const std::string& _receiver)
+{
+    // create a message, a pointer from base to the driven class
+    Message* msg = new TextMessage {_text, username, _receiver};
+
+    // sign message
+    std::string signature = crypto::signMessage(private_key, "my data");
+
+    bool sentmsg = server->create_message(msg, signature);
+    delete msg;
+
+    return sentmsg;
+}
+
+bool User::send_voice_message(const std::string& _receiver)
+{
+    // create a message, a pointer from base to the driven class
+    Message* msg = new VoiceMessage {username, _receiver};
+
+    // sign message
+    std::string signature = crypto::signMessage(private_key, "my data");
+
+    bool sentmsg = server->create_message(msg, signature);
+    delete msg;
+
+    return sentmsg;
+}
